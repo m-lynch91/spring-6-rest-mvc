b207a2ad286bb3bcccffd729b84df5f021694231
Mike Lynch <micheal.j.lynch@gmail.com>
2025-05-15 17:45:00 -0400

    forgot to mark BeerService with @Service annotation

--- src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -1,9 +1,11 @@
 package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Beer;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public interface BeerService {
     Beer getBeerById(UUID id);
 }
