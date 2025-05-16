caebf0d3c71f2feb98d34983564c676df73a231f
Mike Lynch <micheal.j.lynch@gmail.com>
2025-05-16 11:42:18 -0400

    assignment 5 - get customer

--- src/main/java/guru/springframework/spring6restmvc/model/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/model/Beer.java
@@ -20,4 +20,5 @@ public class Beer {
     private BigDecimal price;
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
+
 }
---
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -0,0 +1,58 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Customer;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    private final Map<UUID, Customer> customerMap;
+
+    public CustomerServiceImpl() {
+        customerMap = new HashMap<>();
+
+        Customer cust1 = Customer.builder()
+                .id(UUID.randomUUID())
+                .name("John Doe")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .modifiedDate(LocalDateTime.now())
+                .build();
+        Customer cust2 = Customer.builder()
+                .id(UUID.randomUUID())
+                .name("Jane Doe")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .modifiedDate(LocalDateTime.now())
+                .build();
+        Customer cust3 = Customer.builder()
+                .id(UUID.randomUUID())
+                .name("John Smith")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .modifiedDate(LocalDateTime.now())
+                .build();
+
+        customerMap.put(cust1.getId(), cust1);
+        customerMap.put(cust2.getId(), cust2);
+        customerMap.put(cust3.getId(), cust3);
+    }
+
+    @Override
+    public List<Customer> getCustomers() {
+        return new ArrayList<>(customerMap.values());
+    }
+
+    @Override
+    public Customer getCustomerById(UUID id) {
+        return Customer.builder()
+                .id(id)
+                .name("Uncle Bob")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .modifiedDate(LocalDateTime.now())
+                .build();
+    }
+}
