39a450a0657e037c14f58b88938194ce59464953
Mike Lynch <micheal.j.lynch@gmail.com>
2025-05-30 16:10:55 -0400

    section 8.86 - using @Exception Handler

--- src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
@@ -61,4 +61,9 @@ public class BeerController {
 
         return beerService.getBeerById(beerId);
     }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleNotFoundException() {
+        return ResponseEntity.notFound().build();
+    }
 }
--- src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerTest.java
@@ -117,6 +117,13 @@ class BeerControllerTest {
     }
 
     @Test
+    void getBearByIdNotFound() throws Exception {
+        given(beerService.getBeerById(any(UUID.class))).willThrow(NotFoundException.class);
+        mockMvc.perform(get(BeerController.BEER_PATH_ID, UUID.randomUUID()))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
     void testGetBeerById() throws Exception {
         Beer testBeer = beerServiceImpl.getBeers().getFirst();
 
