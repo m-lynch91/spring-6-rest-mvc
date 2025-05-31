62df82c3e5155347884affdd3b4bac9bebd67f72
Mike Lynch <micheal.j.lynch@gmail.com>
2025-05-31 10:23:14 -0400

    section 9.98 - creating object mappers using MapStruct

---
+++ src/main/java/guru/springframework/spring6restmvc/mappers/BeerMapper.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring6restmvc.mappers;
+
+import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+    // Mapstruct annotation generates all of this code upon Maven.Compile
+    // see /target/generated-sources
+    // ensure that the compiled class has @Component annotation
+    //      if there is not @Component annotation, it could be because of a missing <compilerArg> in the pom.xml
+    //                   <compilerArg>-Amapstruct.defaultComponentModel=spring</compilerArg>
+    Beer beerDtoToBeer(BeerDTO dto);
+    BeerDTO beerToBeerDTO(Beer beer);
+}
