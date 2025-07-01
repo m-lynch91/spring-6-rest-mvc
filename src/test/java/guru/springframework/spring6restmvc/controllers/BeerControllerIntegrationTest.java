e04a3d706fe88de95d0b3f6c638d4ae65429874b
Mike Lynch <micheal.j.lynch@gmail.com>
2025-07-01 16:54:15 -0400

    section 10.119 - jpa validation error message added

--- src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerIntegrationTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerIntegrationTest.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
@@ -24,9 +25,11 @@ import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -76,7 +79,6 @@ class BeerControllerIntegrationTest {
     }
 
 
-
     //---------------------- READ TESTS ------------------------//
     @Test
     void testGetAllBeers() {
@@ -114,7 +116,7 @@ class BeerControllerIntegrationTest {
     @Test
     void testUpdateExistingBeer() {
         Beer beer = beerRepository.findAll().getFirst();
-        BeerDTO beerDTO  = beerMapper.beerToBeerDTO(beer);
+        BeerDTO beerDTO = beerMapper.beerToBeerDTO(beer);
         beerDTO.setId(null);
         beerDTO.setVersion(null);
 
@@ -144,11 +146,15 @@ class BeerControllerIntegrationTest {
         beerMap.put("beerName", "New Name 1234567890123456789012345678901234567890123456789012345678901234567890" +
                 "1234567890123456789012345678901234567890123456789012345678901234567890 ");
 
-        mockMvc.perform(patch(BeerController.BEER_PATH_ID, beer.getId())
+        MvcResult result = mockMvc.perform(patch(BeerController.BEER_PATH_ID, beer.getId())
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(beerMap)))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.length()", is(1)))
+                .andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
     }
 
     //---------------------- DELETE TESTS ----------------------//
