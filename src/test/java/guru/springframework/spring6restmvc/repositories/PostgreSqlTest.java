534b97637b1fd70628c42b762303e05a2d353d7a
Mike Lynch <micheal.j.lynch@gmail.com>
2025-11-26 19:08:07 -0500

    13.143 - using service connection with Test Containers

--- src/test/java/guru/springframework/spring6restmvc/repositories/PostgreSqlTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/PostgreSqlTest.java
@@ -4,6 +4,7 @@ import guru.springframework.spring6restmvc.entities.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.*;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -20,14 +21,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class PostgreSqlTest {
 
     @Container
+    @ServiceConnection
     static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15.3");
-
-    @DynamicPropertySource
-    static void registerPgProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-    }
 
     @Autowired
     BeerRepository beerRepository;
