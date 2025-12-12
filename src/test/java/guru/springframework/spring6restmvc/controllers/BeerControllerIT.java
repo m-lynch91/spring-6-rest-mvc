d51a98f0ec99c6617d1340d6d313c55306db1c9f
Mike Lynch <micheal.j.lynch@gmail.com>
2025-12-12 11:04:57 -0500

    15.156 - added test for list beers by name

--- src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerIT.java
@@ -29,6 +29,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -91,6 +92,14 @@ class BeerControllerIT {
         Beer beer = beerRepository.findAll().getFirst();
         BeerDTO beerDTO = beerController.getBeerById(beer.getId());
         assertNotNull(beerDTO);
+    }
+
+    @Test
+    void testListBeersByName() throws Exception {
+        mockMvc.perform(get(BeerController.BEER_PATH)
+                .queryParam("beerName", "IPA"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(100)));
     }
 
     // SpringBootTest doesn't automatically make this test transactional like with DataJpaTest would
