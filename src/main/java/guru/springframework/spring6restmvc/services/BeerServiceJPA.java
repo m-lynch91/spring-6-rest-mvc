86fe7891f4631bb30446a6bb7d44e44af589b635
Mike Lynch <micheal.j.lynch@gmail.com>
2025-12-17 10:59:42 -0500

    16.169 - added sort parameter

--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -69,7 +70,9 @@ public class BeerServiceJPA implements BeerService {
 			queryPageSize = 1000;
 		}
 
-		return PageRequest.of(queryPageNumber, queryPageSize);
+		Sort sort = Sort.by(Sort.Order.asc("beerName"));
+
+		return PageRequest.of(queryPageNumber, queryPageSize, sort);
 	}
 
 	private Page<Beer> getBeersByNameAndStyle(String beerName, BeerStyle beerStyle, Pageable pageable) {
