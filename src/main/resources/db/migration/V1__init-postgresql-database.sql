53a429727123e9a806d4c755a2bf0a1c461f55bf
Mike Lynch <micheal.j.lynch@gmail.com>
2026-01-09 16:09:04 -0500

    fixed permissions on scripts and added in V1 flyway script. Modified gitignore.

---
+++ src/main/resources/db/migration/V1__init-postgresql-database.sql
@@ -0,0 +1,22 @@
+
+create table beer (
+                      beer_style smallint not null check (beer_style between 0 and 9),
+                      price numeric(38,2) not null,
+                      quantity_on_hand integer,
+                      version integer,
+                      created_date timestamp(6),
+                      update_date timestamp(6),
+                      id varchar(36) not null,
+                      beer_name varchar(50) not null,
+                      upc varchar(255) not null,
+                      primary key (id)
+);
+
+create table customer (
+                          version integer,
+                          created_date timestamp(6),
+                          modified_date timestamp(6),
+                          id varchar(36) not null,
+                          name varchar(255),
+                          primary key (id)
+);
