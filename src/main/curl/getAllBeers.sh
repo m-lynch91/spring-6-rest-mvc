5112b9fe15c0fdc9eb1c6372a9db58ca60652673
Mike Lynch <micheal.j.lynch@gmail.com>
2026-01-09 16:27:47 -0500

    section 22.227 - demonstrated customizing basic auth user name and password

--- src/main/curl/getAllBeers.sh
+++ src/main/curl/getAllBeers.sh
@@ -3,7 +3,7 @@
 status=$(curl -s -o "getAllBeersResponse.json" -w "%{http_code}" -X GET "http://localhost:8080/api/v1/beers" \
       -H "accept: application/json" \
       -H "Content-Type: application/json" \
-      -u user:51d2e4ff-21df-40d1-974b-2160a6a3cb39)
+      -u user1:password)
 
 echo "Response saved to getAllBeersResponse.json"
 echo "HTTP Status: $status"
