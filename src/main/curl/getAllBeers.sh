cab14f2225b21868f6fcb2ccef213e58106773e5
Mike Lynch <micheal.j.lynch@gmail.com>
2026-01-09 16:16:38 -0500

    going to have to update the curl scripts with user:password as needed

--- src/main/curl/getAllBeers.sh
+++ src/main/curl/getAllBeers.sh
@@ -3,7 +3,7 @@
 status=$(curl -s -o "getAllBeersResponse.json" -w "%{http_code}" -X GET "http://localhost:8080/api/v1/beers" \
       -H "accept: application/json" \
       -H "Content-Type: application/json" \
-      -u user:3b3f82eb-fa9b-4edf-aa48-951b41365a5d)
+      -u user:51d2e4ff-21df-40d1-974b-2160a6a3cb39)
 
 echo "Response saved to getAllBeersResponse.json"
 echo "HTTP Status: $status"
