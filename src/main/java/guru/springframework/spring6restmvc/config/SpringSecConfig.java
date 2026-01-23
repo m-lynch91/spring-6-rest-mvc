eda4cb819da5421bb86a914fbe482824038e947e
Mike Lynch <micheal.j.lynch@gmail.com>
2026-01-22 19:33:48 -0500

    section 24.248 - modified spring security configuration

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -11,7 +11,11 @@ public class SpringSecConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests((authorize) ->
+                authorize.anyRequest().authenticated()).oauth2ResourceServer(
+                httpSecurityOAuth2ResourceServerConfigurer -> {
+                    httpSecurityOAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults());
+                });
         http.csrf(httpSecurityCsrfConfigurer -> {
             httpSecurityCsrfConfigurer.ignoringRequestMatchers("/api/**");
         });
